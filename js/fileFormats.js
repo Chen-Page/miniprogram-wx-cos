a823b68b3adc05f8ce0909c7ede130405cbff802
Chen-Page <513805752@qq.com>
2020-08-21 19:39:48 +0800

    测试

---
+++ js/fileFormats.js
@@ -0,0 +1,43 @@
+// 上传文件格式
+
+const audio = [ // 音频格式
+  "mp3",
+  "aac",
+  "flac",
+  "wav",
+  "ogg",
+  "ape",
+  "midi",
+  "ra",
+  "wma",
+  "cda"
+]
+
+const video = [ // 视频格式
+  "mp4",
+  "avi",
+  "mov",
+  "qt",
+  "3gp",
+  "mpeg",
+  "asf",
+  "rm",
+  "navi"
+]
+
+const img = [ // 图片格式
+  "jpg",
+  "png",
+  "gif",
+  "svg",
+  "webp",
+  "jpeg",
+  "ico",
+  "bmp"
+]
+
+module.exports = {
+  audio,
+  video,
+  img
+}
